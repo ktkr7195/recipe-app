b99038b0a70e3a9108de708520bc09ad14df07aa
中村貴幸 <nakamuratakayuki@nakamuratakayuki-no-MacBook-Air.local>
2019-01-03 10:42:27 +0900

    second commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,11 @@ import React, { Component } from "react";
 class Form extends Component {
   state = {};
   render() {
-    return <h1>taka</h1>;
+    return (
+      <form>
+        <input />
+      </form>
+    );
   }
 }
 
